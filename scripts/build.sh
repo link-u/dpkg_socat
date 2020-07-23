c4a48ed065132e600dacff9d34c9f320e478f48e
psi <ryo.hirafuji@link-u.co.jp>
2020-07-24 07:23:27 +0900

    展開

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,10 @@ set -eux
 ## git リポジトリ上の root のパスを取得
 root_dir=$(cd $(dirname $(readlink -f $0)) && cd .. && pwd)
 
-## socat のソースディレクトリ名の取得
-src_dir=$(basename "$(ls -1vd ${root_dir}/socat/socat-*.tar.gz | tail -n 1)" .tar.gz);
+## socat のソースディレクトリ名の取得と展開
+src_filename=$(ls -1vd ${root_dir}/socat/socat-*.tar.gz | tail -n 1)
+src_dir=$(basename "${src_filename}" .tar.gz);
+env --chdir="${root_dir}/socat" tar xvf src_filename
 
 ## debian package のビルド
 env --chdir "${root_dir}/socat"   cp -r debian -t "${src_dir}";
