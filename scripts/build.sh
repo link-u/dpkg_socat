c85daa93aec131ae1b862836d740e643671869d0
psi <ryo.hirafuji@link-u.co.jp>
2020-07-24 07:31:24 +0900

    Fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ root_dir=$(cd $(dirname $(readlink -f $0)) && cd .. && pwd)
 ## socat のソースディレクトリ名の取得と展開
 src_filename=$(ls -1vd ${root_dir}/socat/socat-*.tar.gz | tail -n 1)
 src_dir=$(basename "${src_filename}" .tar.gz);
-env --chdir="${root_dir}/socat" tar xvf src_filename
+env --chdir="${root_dir}/socat" tar xvf ${src_filename}
 
 ## debian package のビルド
 env --chdir "${root_dir}/socat"   cp -r debian -t "${src_dir}";
